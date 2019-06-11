70dd79fc11d5448e5a5644dbd8629fdd91fc93cb
unknown <neilbeukes8@gmail.com>
2019-06-11 09:33:04 +0200

    applied new theme, added new icon, created top status bar scaffolding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { StatusBarComponent } from './layout/status-bar/status-bar.component';
 import { SettingsComponent } from './settings/settings.component';
 import { AboutComponent } from './about/about.component';
 import { HelpComponent } from './help/help.component';
@@ -5,7 +6,8 @@ import { AppRoutingModule } from './routing.module';
 import { MaterialModule } from './shared/material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -18,13 +20,15 @@ import { SidebarComponent } from './layout/sidebar/sidebar.component';
     SidebarComponent,
     HelpComponent,
     AboutComponent,
-    SettingsComponent
+    SettingsComponent,
+    StatusBarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
